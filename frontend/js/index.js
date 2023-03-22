862e9fd1a83b6fe7aa6d2f0f2e59699c93a2c6c8
Nafisa Akter <t2akna00@students.oamk.fi>
2023-03-23 01:47:43 +0600

    commit

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,5 @@
 // const BACKEND_ROOT_URL = 'http://localhost:3001';
-const BACKEND_ROOT_URL = 'https://todo-backend-di39.onrender.com/';
+const BACKEND_ROOT_URL = 'https://todo-backend-di39.onrender.com';
 import { Todos } from './class/Todos.js';
 const todos = new Todos(BACKEND_ROOT_URL);
 const list = document.querySelector('#todolist');
