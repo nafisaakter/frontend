5ced050dd488f70c662a94f699dc244c33bd3617
Nafisa Akter <t2akna00@students.oamk.fi>
2023-03-23 00:42:49 +0600

    'frontend'

---
+++ frontend/ts/class/Todos.ts
@@ -0,0 +1,73 @@
+import { Task } from './Task.js'
+
+class Todos {
+    tasks: Array<Task> = []
+    #backend_url = ''
+    constructor(url:any) {
+        this.#backend_url = url
+    }
+    getTasks = async () => {
+        return new Promise((resolve, reject) => {
+            fetch(this.#backend_url)
+            .then(response => response.json())
+            .then((response) => {
+                this.#readJson(response)
+                resolve(this.tasks)
+            }, (error) => {
+                reject(error)
+            })
+        })  
+    }
+   
+    #readJson(tasksAsJson:any) {
+        tasksAsJson.forEach((node:any) => {
+            const task = new Task(node.id, node.description)
+            this.tasks.push(task)
+        })
+    }
+    #addToArray(id: number, text: string) {
+        const task = new Task(id, text)
+        this.tasks.push(task)
+        return task
+    }
+    addTask = async (text: string) => {
+        return new Promise(async(resolve, reject) => {
+            const json = JSON.stringify({ description: text })
+            fetch(this.#backend_url + "/new", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: json,
+            })
+            .then(response => response.json())
+            .then ((response) => {
+                resolve(this.#addToArray(response.id, text))
+            }), (error) => {
+                reject(error)
+            }
+        })
+
+        }
+    
+
+    #removeFromArray(id: number):void {
+        const arrayWithoutRemoved = this.tasks.filter(task => task.id !== id)
+        this.tasks = arrayWithoutRemoved
+        }
+    removeTask = (id: number) => {
+        return new Promise(async (resolve, reject) => {
+            fetch(this.#backend_url + "/delete/" + id, {
+                method: "DELETE",
+                })
+                .then(response => response.json())
+            .then ((response) => {
+                resolve(this.#removeFromArray(response.id))
+            }), (error) => {
+                reject(error)
+            }
+        })
+
+        }
+}
+export { Todos }
